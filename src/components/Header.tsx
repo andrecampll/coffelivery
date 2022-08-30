6b080b9616bacc175c838798839787141d931298
andrecampll <andrevictor50@gmail.com>
2022-08-30 07:47:50 -0300

    feat: adding Header component 🎨

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { MapPin, ShoppingCart } from 'phosphor-react'
+import { ButtonIcon } from './ButtonIcon'
+
+export const Header = () => (
+  <header className="flex w-full items-center py-8 justify-between">
+    <img src="/logo.svg" alt="logo" />
+
+    <div className="flex items-center gap-4">
+      <div className="flex items-center bg-primary-200 p-2 rounded-md gap-1">
+        <MapPin color="#21A756" weight="fill" size={24} />
+        <span className="text-primary-800 text-sm">Porto Alegre, RS</span>
+      </div>
+
+      <ButtonIcon>
+        <ShoppingCart color="#C47F17" size={24} weight="fill" />
+      </ButtonIcon>
+    </div>
+  </header>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import { Header } from '../components'
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div className="mx-auto my-0 max-w-6xl">
+      <Header />
+    </div>
+  )
 }
