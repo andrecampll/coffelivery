1165c2985470d5ea1c2d48a03e19bd74628026b1
andrecampll <andrevictor50@gmail.com>
2022-08-31 06:51:50 -0300

    chore: cloning element 🛠

--- src/components/BulletPoints.tsx
+++ src/components/BulletPoints.tsx
@@ -1,24 +1,31 @@
 import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
+import { cloneElement } from 'react'
+
+const iconProps = {
+  weight: 'fill',
+  size: 16,
+  color: '#fff',
+}
 
 const POINTS = [
   {
     description: 'Simple and secure purchase',
-    component: <ShoppingCart weight="fill" size={16} color="#fff" />,
+    component: <ShoppingCart />,
     bgColor: 'bg-secondary-800',
   },
   {
     description: 'Packaging keeps the coffee intact',
-    component: <Package weight="fill" size={16} color="#fff" />,
+    component: <Package />,
     bgColor: 'bg-black-500',
   },
   {
     description: 'Fast and tracked delivery',
-    component: <Timer weight="fill" size={16} color="#fff" />,
+    component: <Timer />,
     bgColor: 'bg-secondary-500',
   },
   {
     description: 'Coffee arrives fresh to you',
-    component: <Coffee weight="fill" size={16} color="#fff" />,
+    component: <Coffee />,
     bgColor: 'bg-primary-500',
   },
 ]
@@ -29,7 +36,7 @@ export const BulletPoints = () => {
       {POINTS.map((point) => (
         <div className="flex items-center gap-3" key={point.description}>
           <div className={`rounded-full ${point.bgColor} w-8 p-2`}>
-            {point.component}
+            {cloneElement(point.component, iconProps)}
           </div>
 
           <span className="text-black-500">{point.description}</span>
