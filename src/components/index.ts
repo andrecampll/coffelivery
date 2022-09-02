c6ad244a40e667926297d8e532474935adeef5fb
andrecampll <andrevictor50@gmail.com>
2022-09-01 23:05:16 -0300

    feat: adding product list and coffee card component ⚡️

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export * from './Header'
 export * from './ButtonIcon'
 export * from './Banner'
+export * from './BulletPoints'
+export * from './ProductList'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Header, Banner } from '../components'
+import { Header, Banner, ProductList } from '../components'
 
 export default function Home() {
   return (
     <div className="mx-auto my-0 max-w-6xl px-3">
       <Header />
       <Banner />
+      <ProductList />
     </div>
   )
 }
