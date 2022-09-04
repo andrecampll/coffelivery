0d3899a7840c4f98b99d9b3a8c9c96ef01f81d41
andrecampll <andrevictor50@gmail.com>
2022-09-04 11:01:56 -0300

    chore: adding layouts 🛠

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -60,7 +60,7 @@ const PRODUCTS = [
 ]
 
 export const ProductList = () => (
-  <div className="mt-4">
+  <div className="mt-4 mb-10">
     <h2 className="text-2xl font-bold text-black">Our coffees</h2>
 
     <main className="mt-12 grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
