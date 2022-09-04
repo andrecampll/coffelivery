021228611250bf4eebf83519da2912f8524b3f8c
andrecampll <andrevictor50@gmail.com>
2022-09-04 09:13:30 -0300

    feat: adding coffee card styles 🎨

--- src/components/ButtonIcon.tsx
+++ src/components/ButtonIcon.tsx
@@ -2,8 +2,15 @@ import { ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
+  kind?: 'primary' | 'secondary'
 }
 
-export const ButtonIcon = ({ children }: Props) => (
-  <button className="bg-secondary-200 rounded-md p-2">{children}</button>
+export const ButtonIcon = ({ children, kind = 'primary' }: Props) => (
+  <button
+    className={`${
+      kind === 'primary' ? 'bg-secondary-200' : 'bg-primary-800'
+    } rounded-md p-2`}
+  >
+    {children}
+  </button>
 )
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,17 +1,16 @@
+import { CoffeeCard } from './CoffeeCard'
+
 export const ProductList = () => (
   <div className="mt-4">
     <h2 className="text-2xl font-bold text-black">Our coffees</h2>
 
-    <main className="mt-8 grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-      <h1>coffee</h1>
-      <h1>coffee</h1>
-      <h1>coffee</h1>
-      <h1>coffee</h1>
-      <h1>coffee</h1>
-      <h1>coffee</h1>
-      <h1>coffee</h1>
-      <h1>coffee</h1>
-      <h1>coffee</h1>
+    <main className="mt-12 grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <CoffeeCard
+        name="Traditional express"
+        description="The traditional coffee made with hot water and ground beans"
+        imageUrl="/coffee.svg"
+        price="9.90"
+      />
     </main>
   </div>
 )
