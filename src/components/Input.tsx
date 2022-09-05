da570bdc58a14a61258e81cad9402515b8bface8
andrecampll <andrevictor50@gmail.com>
2022-09-05 05:26:49 -0300

    feat: adding Input form components

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,12 @@
 import { InputHTMLAttributes } from 'react'
 
-export const Input = (props: InputHTMLAttributes<HTMLInputElement>) => (
+type Props = {
+  className?: string
+} & InputHTMLAttributes<HTMLInputElement>
+
+export const Input = ({ className, ...props }: Props) => (
   <input
-    className={`bg-gray p-3 placeholder-gray-800 border border-gray-200 rounded`}
+    className={`bg-gray p-3 placeholder-gray-800 border border-gray-200 rounded ${className}`}
     {...props}
   />
 )
