bc52498eded13ec9e5c3b9dbe8baaac2ffb02a8b
andrecampll <andrevictor50@gmail.com>
2022-09-19 08:57:55 -0300

    feat: adding useGeolocation hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
 import Link from 'next/link'
 import { MapPin, ShoppingCart } from 'phosphor-react'
+import { useGeolocation } from '../hooks'
+
 import { ButtonIcon } from './ButtonIcon'
 
 export const Header = () => {
+  const {
+    address: { city, state },
+  } = useGeolocation()
+
   return (
     <header className="flex w-full items-center py-8 justify-between">
       <Link href="/">
@@ -14,7 +20,9 @@ export const Header = () => {
       <div className="flex items-center gap-4">
         <div className="flex items-center bg-primary-200 p-2 rounded-md gap-1">
           <MapPin color="#21A756" weight="fill" size={24} />
-          <span className="text-primary-800 text-sm">Porto Alegre, RS</span>
+          <span className="text-primary-800 text-sm">
+            {city}, {state}
+          </span>
         </div>
 
         <Link href="/cart">
